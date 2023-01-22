72fd834039cf8722dd8129748fe8faa84dc6c878
Rizqan95 <azqanpenjelajah@gmail.com>
2023-01-22 18:22:46 +0800

    Pemecah stater.html

---
+++ partials/footer.php
@@ -0,0 +1,8 @@
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 0.1.0
+    </div>
+    <strong>Copyright &copy; 2023 <a href="#">Praktikum Web</a>.</strong> All rights reserved.
+  </footer>
